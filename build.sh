321d4e47140f66c56de1f490f4f84218144fb3a8
mohammad hossain <you@example.com>
2018-06-01 17:20:20 +0430

    test

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ cd suricata-3.1
 mkdir builds
 sudo ./configure --prefix=`pwd`/builds/
 sudo make
+sudo make install
 echo "lssssss"
 cd builds/bin/
 a=`ls`
 echo $a
 ls -lah builds
-sudo make install
